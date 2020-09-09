e5de017ded275b0d14aef05b90924a2daaf47574
Austin Cummings <aac729@live.com>
2020-09-09 19:57:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,14 @@
-rrequire_relative 'config/environment'
-require_relative 'models/textanalyzer.rb'
+require_relative 'config/environment'
+require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-get '/' do
-  erb :index
-end
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
-post '/' do
-  text_from_user = params[:user_text]
-  @analyzed_text = TextAnalyzer.new(text_from_user)
-  erb :results
-end
-
-
+    erb :results
+  end
 end
